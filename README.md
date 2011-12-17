560ebbcd70ab30b34a6dc53be8658cbd86f1b401
kishcom <kishcom@underdrive.(none)>
2011-12-16 21:32:41 -0500

    Updated to my version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Impact Socket.IO Plugin
+ImpactJS Socket.io Plugin
 ==========
 
 #### Socket.IO Plugin for the Impact Game Engine ####
@@ -7,32 +7,6 @@ This plugin provides basic realtime multiplayer interaction within games made wi
 
 ### Requirements ###
 
-You'll need NodeJS. A NodeJS based Socket.IO server, and a Javascript Socket.IO client are included in the example app.
+You'll need an impact game to test with. I removed the original example as I am trying to build a more general implementation. I will add a similar demo soon.
 
-You'll also need to accept that this isn't a one-size-fits-all solution. If it works out of the box, that's fantastic!
-
-### Example App ###
-
-Included in this repo is a basic sample app. To get it running, opening a terminal:
-  
-  %> cd path/to/this/repo/example
-  
-  %> node server.js
-  
-Now open up a browser and go to http://localhost:3000
-
-You'll see a dude.
-
-Open up anothe browser and go to http://localhost:3000
-
-In the first browser you'll see a pale looking dude.
-In the second browser you'll still only see one dude. There's a bug there.
-
-In the first browser, press W, S, A, or D. The second browser will now have two dudes, one of them is the dude you're controlling from the first browser.
-
-### Usage ###
-
-1. Study the example app. The various methods in `example/lib/main.js` are pretty crucial.
-2. Copy the impsock.js file into your `lib/plugins/` directory.
-3. Add it to the list of required modules as `plugins.impsock` in your main game file.
 
--- impsock.js
+++ impsock.js
@@ -7,10 +7,9 @@ ig.module(
 .defines(function(){
   ig.Impsock = ig.Class.extend({
     init: function(game) {
-      this.host = null;
-      this.port = 8080;
+      this.host = "http://192.168.2.200:8080";
       this.game = game;
-      this.sessionID = null;
+      this.sessionId = null;
       this.socket = null;
       this.clients = null;
       this.otherClients = null;
@@ -22,26 +21,26 @@ ig.module(
     establishSocket: function() {
       var game = this.game;
       var self = this;
-      this.socket = new io.Socket(this.host,{rememberTransport: false, port: this.port, secure: false});
+      this.socket = io.connect(this.host);
       
       this.socket.on('connect', function(){
-        self.sessionID = self.socket.transport.sessionid;
+        self.sessionId = this.socket.sessionid;
+      });        
+      this.socket.on('message', function(messageFromServer){
+        self.processMessage(messageFromServer);
       });
       
-      this.socket.on('message', function(messageFromServer){
-        self.receive(messageFromServer);
-      });
-      this.socket.connect();
     },
     
     broadcast: function(e) {
       var properties = e.syncableProperties();
-      properties["sessionId"] = e.name;
+      properties.sessionId = e.name;
+
       var message = {"action": "broadcast", "entity": properties};
-      this.socket.send(message);
+      this.socket.emit('message', message);
     },
     
-    receive: function(messageFromServer) {
+    processMessage: function(messageFromServer) {
       if (messageFromServer.spawn) {
         this.game.joinHandler(messageFromServer);
         return;
